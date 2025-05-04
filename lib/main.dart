4958bfef928fe158e87146f7b123bb1e5e243c3d
arafat593 <mdalarafat593@gmail.com>
2025-05-04 14:37:44 +0600

    final check of main

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,9 @@
 
-import 'package:batteryqk_web_app/common/widgets/custom_bottom_navigation_bar.dart';
-import 'package:batteryqk_web_app/features/authentication/views/listings.dart';
-
-
-import 'package:batteryqk_web_app/features/authentication/views/book_screen.dart';
-
-import 'package:batteryqk_web_app/common/widgets/custom_bottom_navigation_bar.dart';
-
-import 'package:batteryqk_web_app/features/authentication/views/points.dart';
-
+import 'package:batteryqk_web_app/batteryqk.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Batteryqk());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-
-      home: Listings(),
-
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        canvasColor: Colors.white,
-        inputDecorationTheme: InputDecorationTheme(
-
-          border: OutlineInputBorder(
-            borderSide: BorderSide.none,
-            borderRadius: BorderRadius.circular(1),
-          ),
-
-        ),
-        textTheme: TextTheme(
-          bodyMedium: TextStyle(
-            color: Color(0xFF374151),
-            fontSize: 14,
-            fontWeight: FontWeight.w500,
-          ),
-        )
-      ),
-
-      title: 'Batteryqk',
-
-
-
-    );
-  }
-}
