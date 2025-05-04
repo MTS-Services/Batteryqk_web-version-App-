8af3d0fe89b7e720a7c4e4b3e5615391a2829092
remon <remonhowlader97@gmail.com>
2025-05-04 15:02:14 +0600

    merge with Listings

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,6 @@
 
-import 'package:batteryqk_web_app/common/widgets/custom_bottom_navigation_bar.dart';
+
 import 'package:batteryqk_web_app/features/authentication/views/listings.dart';
-
-
-import 'package:batteryqk_web_app/features/authentication/views/book_screen.dart';
-
-import 'package:batteryqk_web_app/common/widgets/custom_bottom_navigation_bar.dart';
-
-import 'package:batteryqk_web_app/features/authentication/views/points.dart';
-
 import 'package:flutter/material.dart';
 
 void main() {
