e893aeff6070c50b816eddc13a10b372dd4e8097
Emon Halder <emon.maktech@gmail.com>
2025-05-04 11:37:01 +0600

    local changes

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Batteryqk',
-      home: CustomBottomNavigationBar(),
+      home: CustomBottomNavigationBar(),  
       debugShowCheckedModeBanner: false,
     );
   }
