dac0f7ba2c2c74bce7b633a4d9c768a9d16d4eeb
Emon Halder <emon.maktech@gmail.com>
2025-05-05 09:16:36 +0600

    Car-Service page done

--- lib/features/authentication/views/car_service.dart
+++ lib/features/authentication/views/car_service.dart
@@ -1,18 +1,24 @@
 import 'package:batteryqk_web_app/common/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class CarService extends StatelessWidget {
+  const CarService({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(kToolbarHeight),
         child: CustomAppBar(),
       ),
 
-      body: Text("home page"),
+      body: Center(
+        child: Text(
+          "Coming Soon",
+          style: TextStyle(fontSize: 28, fontWeight: FontWeight.w500),
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,4 @@
-import 'package:batteryqk_web_app/features/authentication/views/book_screen.dart';
-
 import 'package:batteryqk_web_app/common/widgets/custom_bottom_navigation_bar.dart';
-
-import 'package:batteryqk_web_app/features/authentication/views/listings.dart';
-
 import 'package:flutter/material.dart';
 
 void main() {
