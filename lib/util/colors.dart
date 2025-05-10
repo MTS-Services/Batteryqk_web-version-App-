19eab102a08167a9429a40199673acb9a2157d08
remon <remonhowlader97@gmail.com>
2025-05-10 15:46:08 +0600

    color added

--- lib/util/colors.dart
+++ lib/util/colors.dart
@@ -4,4 +4,7 @@ class AppColor {
   static const Color appGreenColor = Color(0xff206CFD);
   static const Color appLightGreenColor = Color(0xffAFDDFF);
   static const Color appLightGreyColor = Color(0xffFBFBFB);
+  static const Color actionColor = Color(0xffFFA726);
+  static const Color mainTBColor = Color(0xff212121);
+  static const Color iconColor = Color(0xff00BCD4);
 }
