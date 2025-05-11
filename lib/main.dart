6b110bdd9a74dbfc87d0ef4c5ff1ac11359727e4
arafat593 <mdalarafat593@gmail.com>
2025-05-11 08:47:05 +0600

    listing updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'package:batteryqk_web_app/common/widgets/custom_bottom_navigation_bar.dart';
-import 'package:batteryqk_web_app/features/authentication/views/book_screen.dart';
-import 'package:batteryqk_web_app/features/authentication/views/listings.dart';
 import 'package:batteryqk_web_app/features/authentication/views/login_screen.dart';
-import 'package:batteryqk_web_app/features/authentication/views/welcome_screen.dart';
 import 'package:batteryqk_web_app/util/colors.dart';
 import 'package:flutter/material.dart';
 
