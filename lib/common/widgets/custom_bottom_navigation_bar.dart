9a3ad825b19329badd82f529ad222747839644c8
arafat593 <mdalarafat593@gmail.com>
2025-05-11 11:54:52 +0600

    bottom bar added by listing

--- lib/common/widgets/custom_bottom_navigation_bar.dart
+++ lib/common/widgets/custom_bottom_navigation_bar.dart
@@ -1,9 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:batteryqk_web_app/features/authentication/views/book_screen.dart';
 import 'package:batteryqk_web_app/features/authentication/views/home_screen.dart';
 import 'package:batteryqk_web_app/features/authentication/views/listings.dart';
 import 'package:batteryqk_web_app/features/authentication/views/menu_screen.dart';
 import 'package:batteryqk_web_app/util/colors.dart';
-import 'package:flutter/material.dart';
 
 class CustomBottomNavigationBar extends StatefulWidget {
   const CustomBottomNavigationBar({super.key});
@@ -15,7 +15,6 @@ class CustomBottomNavigationBar extends StatefulWidget {
 
 class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
   int _currentIndex = 0;
-
   final List<Widget> _pages = [
     const HomeScreen(),
     const Listings(),
@@ -26,60 +25,53 @@ class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AnimatedSwitcher(
-        duration: const Duration(milliseconds: 300),
-        child: _pages[_currentIndex],
-      ),
+      extendBody: true,
+      body: _pages[_currentIndex],
       bottomNavigationBar: Container(
+        margin: const EdgeInsets.all(16),
+        height: 70,
         decoration: BoxDecoration(
-          color: Colors.white,
+          color: Colors.white.withOpacity(0.9),
+          borderRadius: BorderRadius.circular(30),
           boxShadow: [
             BoxShadow(
-              blurRadius: 8,
-              color: Colors.black12,
-              offset: Offset(0, -2),
+              color: Colors.black.withOpacity(0.06),
+              blurRadius: 12,
+              offset: const Offset(0, 4),
             ),
           ],
         ),
-        child: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          currentIndex: _currentIndex,
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          backgroundColor: Colors.white,
-          selectedItemColor: AppColor.blueColor,
-          unselectedItemColor: Colors.grey.shade500,
-          selectedFontSize: 14,
-          unselectedFontSize: 13,
-          showUnselectedLabels: true,
-          elevation: 1,
-          iconSize: 26,
-          selectedLabelStyle: const TextStyle(
-            fontWeight: FontWeight.w500,
-            letterSpacing: 0.2,
-          ),
-          unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.w500),
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home_outlined),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.assignment_outlined),
-              label: 'Listings',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_today_outlined),
-              label: 'Book',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.menu_rounded),
-              label: 'Menu',
-            ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _buildIcon(icon: Icons.home_rounded, index: 0),
+            _buildIcon(icon: Icons.assignment_outlined, index: 1),
+            _buildIcon(icon: Icons.calendar_today_outlined, index: 2),
+            _buildIcon(icon: Icons.menu_rounded, index: 3),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildIcon({required IconData icon, required int index}) {
+    final bool isSelected = _currentIndex == index;
+    return GestureDetector(
+      onTap: () => setState(() => _currentIndex = index),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 250),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        decoration: BoxDecoration(
+          color:
+              isSelected
+                  ? AppColor.blueColor.withOpacity(0.15)
+                  : Colors.transparent,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Icon(
+          icon,
+          size: 26,
+          color: isSelected ? AppColor.blueColor : Colors.grey.shade600,
         ),
       ),
     );
