6fb3f277c3fd7059d88493e55999450834f7189f
arafat593 <mdalarafat593@gmail.com>
2025-05-11 12:31:59 +0600

    main is updated

--- lib/common/widgets/custom_app_bar.dart
+++ lib/common/widgets/custom_app_bar.dart
@@ -1,45 +1,59 @@
-import 'package:batteryqk_web_app/util/colors.dart';
-import 'package:batteryqk_web_app/util/images_path.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:batteryqk_web_app/util/colors.dart';
+import 'package:batteryqk_web_app/util/images_path.dart';
 
-class CustomAppBar extends StatelessWidget {
-  bool? isBack = false;
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final bool isBack;
 
-  CustomAppBar({super.key , this.isBack});
+  CustomAppBar({super.key, this.isBack = false});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(70);
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      automaticallyImplyLeading: isBack!,
-      toolbarHeight: 70,
-      backgroundColor: Colors.white,
-      elevation: 1,
-      titleSpacing: 4,
-      title: Padding(
-        padding: const EdgeInsets.only(left: 15),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            
-            Image.asset(AppImages.logo,height: 100, width: 60,),
-            Text(
-              'Batteriqk',
-              style: GoogleFonts.openSans(
-                letterSpacing: 1.2,
-                fontSize: 28,
-                fontWeight: FontWeight.w900,
-                color: AppColor.blackColor,
-              ),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: Container(
+        height: 100,
+        color: Colors.white, // ✅ Force solid white background
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              children: [
+                if (isBack)
+                  IconButton(
+                    icon: const Icon(Icons.arrow_back, color: Colors.black),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                if (!isBack) const SizedBox(width: 0),
+                Image.asset(AppImages.logo, height: 50, width: 65),
+                const SizedBox(width: 5),
+                Text(
+                  'Batteriqk',
+                  style: GoogleFonts.openSans(
+                    letterSpacing: 1.2,
+                    fontSize: 22,
+                    fontWeight: FontWeight.w900,
+                    color: AppColor.blackColor,
+                  ),
+                ),
+                const Spacer(),
+                IconButton(
+                  icon: const Icon(
+                    Icons.notifications_none,
+                    color: Colors.black87,
+                    size: 26,
+                  ),
+                  onPressed: () {
+                    // TODO: Handle notification click
+                  },
+                ),
+              ],
             ),
-            SizedBox(width: 20),
-            IconButton(
-              icon: const Icon(Icons.notifications_none, color: Colors.black87),
-              onPressed: () {
-                // Handle notification tap
-              },
-            ),
-          ],
+          ),
         ),
       ),
     );
--- lib/common/widgets/custom_bottom_navigation_bar.dart
+++ lib/common/widgets/custom_bottom_navigation_bar.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:batteryqk_web_app/features/authentication/views/book_screen.dart';
 import 'package:batteryqk_web_app/features/authentication/views/home_screen.dart';
@@ -13,8 +14,10 @@ class CustomBottomNavigationBar extends StatefulWidget {
       _CustomBottomNavigationBarState();
 }
 
-class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
+class _CustomBottomNavigationBarState
+    extends State<CustomBottomNavigationBar> {
   int _currentIndex = 0;
+
   final List<Widget> _pages = [
     const HomeScreen(),
     const Listings(),
@@ -27,51 +30,77 @@ class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
     return Scaffold(
       extendBody: true,
       body: _pages[_currentIndex],
-      bottomNavigationBar: Container(
-        margin: const EdgeInsets.all(16),
-        height: 70,
-        decoration: BoxDecoration(
-          color: Colors.white.withOpacity(0.9),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(16),
+        child: ClipRRect(
           borderRadius: BorderRadius.circular(30),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.06),
-              blurRadius: 12,
-              offset: const Offset(0, 4),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
+            child: Container(
+              height: 70,
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.85),
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.07),
+                    blurRadius: 20,
+                    offset: const Offset(0, 6),
+                  ),
+                ],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _buildNavItem(Icons.home_rounded, 0),
+                  _buildNavItem(Icons.assignment, 1),
+                  _buildNavItem(Icons.bookmarks_outlined, 2),
+                  _buildNavItem(Icons.person_rounded, 3),
+                ],
+              ),
             ),
-          ],
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            _buildIcon(icon: Icons.home_rounded, index: 0),
-            _buildIcon(icon: Icons.assignment_outlined, index: 1),
-            _buildIcon(icon: Icons.calendar_today_outlined, index: 2),
-            _buildIcon(icon: Icons.menu_rounded, index: 3),
-          ],
+          ),
         ),
       ),
     );
   }
 
-  Widget _buildIcon({required IconData icon, required int index}) {
+  Widget _buildNavItem(IconData icon, int index) {
     final bool isSelected = _currentIndex == index;
+
     return GestureDetector(
-      onTap: () => setState(() => _currentIndex = index),
+      onTap: () {
+        setState(() => _currentIndex = index);
+        // Optional: Add tap feedback
+        // Feedback.forTap(context);
+      },
       child: AnimatedContainer(
-        duration: const Duration(milliseconds: 250),
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        duration: const Duration(milliseconds: 300),
+        padding: const EdgeInsets.all(10),
         decoration: BoxDecoration(
-          color:
-              isSelected
-                  ? AppColor.blueColor.withOpacity(0.15)
-                  : Colors.transparent,
+          color: isSelected
+              ? AppColor.blueColor.withOpacity(0.12)
+              : Colors.transparent,
           borderRadius: BorderRadius.circular(20),
+          boxShadow: isSelected
+              ? [
+            BoxShadow(
+              color: AppColor.blueColor.withOpacity(0.3),
+              blurRadius: 12,
+              offset: const Offset(0, 4),
+            )
+          ]
+              : [],
         ),
-        child: Icon(
-          icon,
-          size: 26,
-          color: isSelected ? AppColor.blueColor : Colors.grey.shade600,
+        child: AnimatedScale(
+          scale: isSelected ? 1.25 : 1.0,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+          child: Icon(
+            icon,
+            size: 26,
+            color: isSelected ? AppColor.blueColor : Colors.grey.shade500,
+          ),
         ),
       ),
     );
